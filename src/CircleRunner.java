69f908e8bebaf1e4857a287170d5dbe8bc64a373
Anna Chen <annac101@nycstudents.net>
2022-09-30 08:28:56 -0400

    commit 1

--- src/CircleRunner.java
+++ src/CircleRunner.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class CircleRunner {
+public class CircleRunner {
+    public static void main(String[] args) {
+        // A. create a Circle object with radius 5.0
+        Circle circle1= new Circle(0.5);
+
+        // B. locate the method defined in the Circle class and write
+        //    a line of code to call it on your object
+        circle1.printArea();
+
+        // C. BEFORE running this code, PREDICT what gets printed
+        //It's going to print My area is: 0.785.
+        // D. run the code and confirm your prediction! Were you correct?
+        //Yes I was.
+    }
 }
